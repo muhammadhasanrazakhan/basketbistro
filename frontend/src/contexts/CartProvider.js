9ca4110ecd70da4eb03871b5afaefbe8778c1013
muhammadhasanrazakhan <129754245+muhammadhasanrazakhan@users.noreply.github.com>
2025-09-30 03:37:51 +0500

    Delete frontend directory

--- frontend/src/contexts/CartProvider.js
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from 'react';
-import useCart from '../hooks/useCart';
-
-export const CartContext = createContext();
-
-const CartProvider = ({ children }) => <CartContext.Provider value={useCart()}>{children}</CartContext.Provider>;
-
-export default CartProvider;
